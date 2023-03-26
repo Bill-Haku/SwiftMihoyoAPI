f23f749dfe464ef185fbc77b4ac3aedc8585e03d
Bill-Haku <battleandwar@hotmail.com>
2023-03-26 09:40:20 +0800

    feat: move files to HBMihoyoAPIPackage

--- Tests/HBMihoyoAPITests/HBMihoyoAPITests.swift
+++ Tests/HBMihoyoAPITests/HBMihoyoAPITests.swift
@@ -6,6 +6,5 @@ final class HBMihoyoAPITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(HBMihoyoAPI().text, "Hello, World!")
     }
 }
